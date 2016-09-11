579dd5fe552744dad594465c56863aef82a981bd
haiverg <haiver.galeano@gmail.com>
2016-09-11 17:20:16 -0500

    First commit

---
+++ hgp-dev-deploy/src/main/java/com/redhat/training/hgp_dev_deploy/App.java
@@ -0,0 +1,13 @@
+package com.redhat.training.hgp_dev_deploy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
